bad3a411df1e03c55e3131df67e59b1fb96ad113
HuyBeeOne <quang.huybeeone@gmail.com>
2023-12-01 22:14:29 +0700

    fist commit (setup - header sidebar component)

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import ButtonUsage from './components/ButtonUsage';
+import Header from './components/Header';
+import Layout from './components/Layout';
+
+function App() {
+
+  return (
+    <>
+      <Layout/>
+    </>
+  )
+}
+
+export default App
