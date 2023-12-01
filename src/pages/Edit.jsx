6642f619110081a617a76ab121a5f749a4c3aabf
HuyBeeOne <quang.huybeeone@gmail.com>
2023-12-01 23:56:23 +0700

    upload Edit

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { editContact } from '../redux/contactsReducer';
-
+import './Edit.css'
 const Edit = () => {
   const { index } = useParams();
   const navigate = useNavigate();
@@ -25,42 +25,51 @@ const Edit = () => {
 
   return (
     <div>
-      <h1>Edit Contact #{index}</h1>
-      <form>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={contact.name}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="text"
-            name="email"
-            value={contact.email}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <label>
-          Phone Number:
-          <input
-            type="text"
-            name="phoneNumber"
-            value={contact.phoneNumber}
-            onChange={handleChange}
-          />
-        </label>
-        <br />
-        <button type="button" onClick={handleSave}>
-          Save
-        </button>
-      </form>
+  <h1>Edit Contact #{index}</h1>
+  <form>
+    <label>
+      Name:
+      <input
+        type="text"
+        name="name"
+        value={contact.name}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </label>
+    <br />
+    <label>
+      Email:
+      <input
+        type="text"
+        name="email"
+        value={contact.email}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </label>
+    <br />
+    <label>
+      Phone Number:
+      <input
+        type="text"
+        name="phoneNumber"
+        value={contact.phoneNumber}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </label>
+    <br />
+    <Link to={'/'}>
+      <button type="button" className="form-button cancel">
+        Cancel
+      </button>
+    </Link>
+
+    <button type="button" onClick={handleSave} className="form-button">
+      Save
+    </button>
+  </form>
     </div>
   );
 };
